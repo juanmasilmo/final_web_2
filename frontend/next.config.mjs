262f1053993995b33627e5dc40d1c8c638625e3d
jmsilvamolina <jmsilvamonia@jusmisiones.gov.ar>
2026-08-25 21:12:39 -0300

    se corrige config.mjs

---
+++ frontend/next.config.mjs
@@ -0,0 +1,8 @@
+const nextConfig = {
+  // reactStrictMode: activa advertencias adicionales de React en desarrollo
+    reactStrictMode: true,
+};
+
+export default(phase,{defaultConfig})=>{
+return nextConfig;
+}
--- frontend/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  // reactStrictMode: activa advertencias adicionales de React en desarrollo
-  reactStrictMode: true,
-};
-
-export default nextConfig;
